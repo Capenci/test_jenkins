0e58b10f9c0d3a83b11ad028a90211086ec56b9e
capenci <nguyencongtruongpt99@gmail.com>
2025-04-14 16:40:46 +0700

    commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+class HelloHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        message = "Hello, World!"
+        self.send_response(200)
+        self.send_header("Content-type", "text/plain")
+        self.end_headers()
+        self.wfile.write(message.encode())
+
+if __name__ == "__main__":
+    server = HTTPServer(("0.0.0.0", 8000), HelloHandler)
+    print("Serving on port 8000")
+    server.serve_forever()
